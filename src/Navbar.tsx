4c2549f9a62f5e28e60b8c24a8eb6c4148feddbe
mslmyrtd <mslmyrtd@gmail.com>
2023-01-02 14:43:09 +0300

    navbar added new button and class

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useGlobalContext } from './context'
+import logo from './images/logo.svg'
+import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const { openSidebar } = useGlobalContext()
+    return (
+        <nav className='nav'>
+            <div className='nav-center'>
+                <div className='nav-header'>
+                    <img src={logo} alt='stripe' className='nav-logo' />
+                    <button className='btn toggle-btn' onClick={openSidebar}>
+                        <FaBars />
+                    </button>
+                </div>
+                <ul className='nav-links'>
+                    <li>
+                        <button className='link-btn'>products</button>
+                    </li>
+                    <li>
+                        <button className='link-btn'>developers</button>
+                    </li>
+                    <li>
+                        <button className='link-btn'>company</button>
+                    </li>
+                </ul>
+                <button className='btn signin-btn'>Sign in</button>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
