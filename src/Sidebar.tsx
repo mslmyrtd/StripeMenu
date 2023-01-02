4d63471f980af9958e2a8e476349bc4d3f01f9a6
mslmyrtd <mslmyrtd@gmail.com>
2023-01-02 15:44:19 +0300

    sidebar structure created and added sublinks and context

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import { useGlobalContext } from './context'
+import sublinks from './data'
+import { FaTimes } from 'react-icons/fa'
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
   return (
-    <div>
-      
-    </div>
+    <aside
+      className={`${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+        }`}
+    >
+      <div className='sidebar'>
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'>
+          {sublinks.map((item: any, index: number): any => {
+            const { links, page } = item
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link: any, index: number) => {
+                    const { url, icon, label } = link
+                    return (
+                      <a href={url} key={index}>
+                        {icon}
+                        {label}
+                      </a>
+                    )
+                  })}
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </aside>
   )
 }
 
