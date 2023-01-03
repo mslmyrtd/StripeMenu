a1d8c7ebc4d2484add48fff9ede1a0da6a797808
mslmyrtd <mslmyrtd@gmail.com>
2023-01-03 14:35:24 +0300

    displaySubMenu function added but i must make some changes type

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,9 +1,17 @@
-import { useGlobalContext } from './context'
-import logo from './images/logo.svg'
+import { useGlobalContext } from './context/context'
 import { FaBars } from 'react-icons/fa'
+import logo from './images/logo.svg'
 
 const Navbar = () => {
-    const { openSidebar } = useGlobalContext()
+    const { openSidebar, openSubmenu } = useGlobalContext()
+    const displaySubmenu = (e: any) => {
+        const page = e.target.textContent
+        const tempBtn = e.target.getBoundingClientRect()
+        const center = (tempBtn.left + tempBtn.right) / 2;
+        const bottom = tempBtn.bottom - 3
+        openSubmenu(page, { center, bottom })
+
+    }
     return (
         <nav className='nav'>
             <div className='nav-center'>
@@ -15,13 +23,13 @@ const Navbar = () => {
                 </div>
                 <ul className='nav-links'>
                     <li>
-                        <button className='link-btn'>products</button>
+                        <button className='link-btn' onMouseOver={displaySubmenu}>products</button>
                     </li>
                     <li>
-                        <button className='link-btn'>developers</button>
+                        <button className='link-btn' onMouseOver={displaySubmenu}>developers</button>
                     </li>
                     <li>
-                        <button className='link-btn'>company</button>
+                        <button className='link-btn' onMouseOver={displaySubmenu}>company</button>
                     </li>
                 </ul>
                 <button className='btn signin-btn'>Sign in</button>
