82d5c92ae0b12b61e730445a8b760a26413b3b29
mslmyrtd <mslmyrtd@gmail.com>
2023-01-03 14:34:34 +0300

    useRef added for add style and some function added

--- src/Submenu.tsx
+++ src/Submenu.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useGlobalContext } from './context/context'
 
 const Submenu = () => {
+  const { isSubmenuOpen, location } = useGlobalContext()
+  const container = useRef<HTMLInputElement>(null)
+  console.log(container.current);
+  useEffect(() => {
+    const submenu = container.current;
+    const { center, bottom } = location
+    if (submenu) {
+      submenu.style.left = `${center}px`
+      submenu.style.top = `${bottom}px`
+
+    }
+    // console.log(submenu.style.left);
+  }, [location])
+
   return (
-    <div>
-      
-    </div>
+    <aside className={`${isSubmenuOpen ? "submenu show " : "submenu"}`} ref={container}>
+      submenu
+    </aside>
   )
 }
 
