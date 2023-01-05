420807ae48e7975e86a434cf31c3411008b4ce82
mslmyrtd <mslmyrtd@gmail.com>
2023-01-05 21:31:41 +0300

    depending on amount oflinkschanged the amount of columns

--- src/Submenu.tsx
+++ src/Submenu.tsx
@@ -1,26 +1,32 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useGlobalContext } from './context/context'
 
 const Submenu = () => {
   const { isSubmenuOpen, location, page: { page, links } } = useGlobalContext()
   const container = useRef<HTMLInputElement>(null)
+  const [columns, setColumns] = useState('col-2')
   useEffect(() => {
+    setColumns("col-2")
     const submenu = container.current;
     const { center, bottom } = location
     if (submenu) {
       submenu.style.left = `${center}px`
       submenu.style.top = `${bottom}px`
-
     }
-    // console.log(submenu.style.left);
-  }, [location])
+    if (links.length === 3) {
+      setColumns("col-3")
+    }
+    if (links.length > 3) {
+      setColumns("col-4")
+    }
+
+  }, [location, links])
 
   return (
     <aside className={`${isSubmenuOpen ? "submenu show " : "submenu"}`} ref={container}>
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`}>
+      <div className={`submenu-center ${columns}`}>
         {links.map((link: any, index: number) => {
-          console.log(typeof link);
           const { label, icon, url } = link
           return (<a key={index} href={url}>{icon}{label}</a>)
         })}
