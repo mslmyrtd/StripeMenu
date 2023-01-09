d18c1c4539fb62a2dc091ce9779bc56380194700
mslmyrtd <mslmyrtd@gmail.com>
2023-01-09 09:48:38 +0300

    added onMouseOver

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   const { closeSubmenu } = useGlobalContext()
 
   return (
-    <section className='hero'>
+    <section className='hero' onMouseOver={closeSubmenu}>
       <div className="hero-center">
         <article className='hero-info'>
           <h1>Payments infrastructure
